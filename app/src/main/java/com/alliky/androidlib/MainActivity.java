cf77eda6e6ef3425f56245ce9bb2366e70f23293
wxianing <Lianing.00>
2019-09-19 19:48:29 +0800

    111

--- app/src/main/java/com/alliky/androidlib/MainActivity.java
+++ app/src/main/java/com/alliky/androidlib/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.Toast.makeText("<>>>>", "0000", Toast.LENGTH_SHORT).show();
     }
 }
